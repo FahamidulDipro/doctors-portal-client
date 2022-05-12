fa283a680587fce660d98ddbf01a72d62e1876f6
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-05-13 00:51:02 +0600

    footer added

--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import Banner from './Banner';
-import Info from './Info';
-import MakeAppoinment from './MakeAppoinment';
-import Services from './Services';
-import Testimonials from './Testimonials';
+import React from "react";
+import Banner from "./Banner";
+import Footer from "./Footer";
+import Info from "./Info";
+import MakeAppoinment from "./MakeAppoinment";
+import Services from "./Services";
+import Testimonials from "./Testimonials";
 
 const Home = () => {
-    return (
-        <div className='px-12'>
-           <Banner></Banner>
-           <Info></Info>
-           <Services></Services>
-           <MakeAppoinment></MakeAppoinment>
-           <Testimonials></Testimonials>
-        </div>
-    );
+  return (
+    <div>
+      <div className="px-12">
+        <Banner></Banner>
+        <Info></Info>
+        <Services></Services>
+        <MakeAppoinment></MakeAppoinment>
+        <Testimonials></Testimonials>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
