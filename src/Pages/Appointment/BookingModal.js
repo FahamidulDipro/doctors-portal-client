d90305e21804bce95e73da74a756d2a4f024a7de
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-05-14 02:14:23 +0600

    firebase init.js files are saved into .env.local file

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { format } from "date-fns";
 const BookingModal = ({ date, treatment }) => {
-  const { name, slots } = treatment;
+  const { _id, name, slots } = treatment;
+  const handleBooking = (event) => {
+    event.preventDefault();
+    const slot = event.target.slot.value;
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value;
+    console.log(_id, slot, name, email, phone);
+  };
   return (
     <div>
       <input type="checkbox" id="booking-modal" class="modal-toggle" />
@@ -16,46 +24,42 @@ const BookingModal = ({ date, treatment }) => {
           <h3 class="text-lg font-bold">
             Booking For: <span className="text-secondary">{name}</span>
           </h3>
-          <form>
+          <form onSubmit={handleBooking}>
             <input
               type="text"
               value={format(date, "PP")}
               disabled
-              class="input input-bordered w-full max-w-xs my-1"
+              class="input input-bordered w-full max-w-xs my-2"
             />
-            <select class="select select-bordered w-full max-w-xs">
-              <option disabled selected>
-                Who shot first?
-              </option>
+            <select name="slot" class="select select-bordered w-full max-w-xs">
               {slots.map((slot) => (
-                <option>{slot}</option>
+                <option key={_id}>{slot}</option>
               ))}
             </select>
             <input
               type="text"
-              placeholder="Type here"
-              class="input input-bordered w-full max-w-xs my-1"
+              name="name"
+              placeholder="Your Name"
+              class="input input-bordered w-full max-w-xs my-2"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              class="input input-bordered w-full max-w-xs my-2"
             />
             <input
               type="text"
-              placeholder="Type here"
-              class="input input-bordered w-full max-w-xs my-1"
-            />
-            <input
-              type="text"
-              placeholder="Type here"
-              class="input input-bordered w-full max-w-xs my-1"
+              name="phone"
+              placeholder="Phone Number"
+              class="input input-bordered w-full max-w-xs my-2"
             />
             <input
               type="submit"
               value="SUBMIT"
-              class="input input-bordered w-full max-w-xs my-1 bg-secondary text-white cursor-pointer"
+              class="input input-bordered w-full max-w-xs my-1 bg-accent text-white cursor-pointer"
             />
           </form>
-          <p class="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
         </div>
       </div>
     </div>
